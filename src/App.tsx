434103e9463568584904c4681eb7f7296f62f91d
tamjohn <tamjohnathan@gmail.com>
2024-02-07 15:16:40 -0800

    added work experience page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { NavBar } from "./components/NavigationBar";
 import Banner from "./components/Banner";
 import { HomePageCards } from "./components/HomePageCards";
 import { AboutMePage } from "./components/AboutMe";
+import { WorkExp } from "./components/WorkExp";
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
       </section>
       <section id="about" className="sectionContainer">
         <AboutMePage />
+      </section>
+      <section id='work' className="sectionContainer">
+        <WorkExp />
       </section>
     </div>
   );
---
+++ src/components/WorkExp.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, Tab } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import diligentLogo from '../assets/img/Diligent_Logo.jpg';
+import deltaLogo from '../assets/img/DeltaControls_Logo.png';
+import convergenceLogo from '../assets/img/Convergence_logo.jpg';
+import arcLogo from '../assets/img/ARC_logo.png';
+
+type cardType = {
+    companyName: string;
+    role: string;
+    datesWorked: string;
+    techStack: string[];
+    logoUrl: string;
+    link: string;
+};
+
+export const WorkExp: React.FC = () => {
+  const Cards: cardType[] = [
+    {
+      companyName: "Diligent Corporation",
+      role: "Software Engineer Intern",
+      datesWorked: "Sep 2023 - Jan 2024",
+      techStack: ["React", "TypeScript", "Node.js"],
+      logoUrl: diligentLogo,
+      link: "/#company-one",
+    },
+    {
+        companyName: "Delta Controls",
+        role: "Junior Software Developer Intern",
+        datesWorked: "May 2023 - Aug 2023",
+        techStack: ["React", "TypeScript", "Node.js"],
+        logoUrl: deltaLogo,
+        link: "/#company-one",
+    },
+    {
+        companyName: "Convergence Concepts",
+        role: "Full Stack Software Developer Intern",
+        datesWorked: "Sep 2022 - Dec 2022",
+        techStack: ["React", "TypeScript", "Node.js"],
+        logoUrl: convergenceLogo,
+        link: "/#company-one",
+    },
+    {
+        companyName: "Arthritis Research Canada",
+        role: "Research Coordinator",
+        datesWorked: "May 2017 - Sep 2022",
+        techStack: ["React", "TypeScript", "Node.js"],
+        logoUrl: arcLogo,
+        link: "/#company-one",
+    },
+  ];
+
+  return (
+    <section className="workExpPage" id='work-exp'>
+      <Container className='workExpCardsContainer'>
+      <Row>
+        <Col className="workExpHeader">
+          <h2><strong>Work Experience</strong></h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="resumeLink">
+          <a href="URL_TO_YOUR_PDF" target="_blank" rel="noopener noreferrer">
+            View My Resume
+          </a>
+        </Col>
+      </Row>
+        <Row>
+          {Cards.map((card, index) => (
+            <Col xs={12} sm={6} md={3} lg={3} key={index}>
+              <Link to={card.link} style={{ textDecoration: 'none' }}>
+                <Card className="h-100 workCard">
+                  <div className='imageContainer'>
+                    <Card.Img variant="top" src={card.logoUrl} className='workCardImg' />
+                  </div>
+                  <Card.Body>
+                    <Card.Title>{card.companyName}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{card.role}</Card.Subtitle>
+                    <Card.Text>
+                      <small>{card.datesWorked}</small>
+                    </Card.Text>
+                    <Card.Text>
+                      Tech Stack: {card.techStack.join(', ')}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </section>
+  );
+};
